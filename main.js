21a7213d1c974a1b7d6bf89deceb586bb30688c8
Meuryss <133385697+Meuryss@users.noreply.github.com>
2024-04-11 20:22:53 -0300

    Aula 6 11/4

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-const texto = document.querySelectorAll(".aba-conteudo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i=0; i < botoes.length; i++){
     botoes[i].onclick = function() {
@@ -11,13 +11,17 @@ for (let i=0; i < botoes.length; i++){
         botoes[i].classList.add("ativo");
         botoes[i].classList.add("ativo");
     }
+
 }
 
+
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-05-10T23:59:59");
+const tempoObjetivo1 = new Date("2024-04-30T23:59:59");
+const tempoObjetivo2 = new Date("2024-05-10T23:59:59");
+const tempoObjetivo3 = new Date("2024-07-15T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-10T23:59:59");
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
