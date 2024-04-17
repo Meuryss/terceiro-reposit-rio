f620019187c9670be0bea0c65bbc3033d4274ae4
Meuryss <133385697+Meuryss@users.noreply.github.com>
2024-04-16 22:44:04 -0300

    Aula 7 16/04

--- main.js
+++ main.js
@@ -34,8 +34,29 @@ let dias = Math.floor(horas/24);
 
 segundos %= 60;
 minutos %= 60;
-horas %24= 24;
+horas %= 24;
 
-return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+if (tempoFinal > 0) {
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+} else {
+    return "Prazo finalizado";
 }
+
+}
+
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro(),1000);
+}
+
+comecaCronometro();
+
+
 
