8f02f6a52f22e21fd8a7f16327171055f780cdfa
Meuryss <133385697+Meuryss@users.noreply.github.com>
2024-04-23 22:40:21 -0300

    AULA 8

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function atualizaCronometro() {
 
 function comecaCronometro(){
     atualizaCronometro();
-    setInterval(atualizaCronometro(),1000);
+    setInterval(atualizaCronometro,1000);
 }
 
 comecaCronometro();
